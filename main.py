6e01aaed69335779148ad6c0c5731bba627745f3
kariebik <kekatimi@gmail.com>
2023-12-19 10:32:48 -0500

    main update.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def main_message():
+from Group_work import fibonacci
+from mendy import factorial
+
+
+if __name__ =='__main__':
     print("which task would you like to perform: ")
     print("1: Find the factorial")
     print("2: Find the fibonacci value")
-    input()
+    e = eval(input("Enter the number of the task you'd like to perform: "))
+
+    if e == 1:
+        k = eval(input("Enter the integer you'd like like to perform: "))
+        print("The factorial of " + str(k) + "is:" + factorial(k))

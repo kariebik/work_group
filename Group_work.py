ec5ee64da3a000b2c8bda0e94419b33e96d2ceb4
LevPPL <levacuber@gmail.com>
2023-12-19 09:57:03 -0500

    Fibonacci initial push

---
+++ Group_work.py
@@ -0,0 +1,18 @@
+
+def fibonacci(k):
+    if k in [0, 1]:
+        return k
+    return False
+
+
+#     l = k+2
+#     if l < 0:
+#         raise ValueError
+#     elif l == 0:
+#         return "0"
+#     elif l == 1:
+#         return "1"
+#     else:
+#         l = l*(l - 1) + l*(l - 2)
+#         return l
+# print(fibonacci(8))

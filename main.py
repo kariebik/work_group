acf57b3347cc1f39feba90558cc3ac44fda7e509
kariebik <kekatimi@gmail.com>
2023-12-19 11:20:57 -0500

    main update.

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ if __name__ =='__main__':
         k = eval(input("Enter the integer you'd like like to perform: "))
         if not isinstance(k, int):
             raise TypeError
-        print("The factorial of " + str(k) + "is:" + factorial(k))
+        print("The factorial of " + str(k) + " is: " + str(factorial(k)))
     elif e == 2:
         n = eval(input("Enter the sequence position of the fibonacci value: "))
         if not isinstance(n, int):
             raise TypeError
-        print("The " + str(n) + "th fibonacci is " + fibonacci_sequence(n))
+        print("The " + str(n) + " th fibonacci is " + str(fibonacci_sequence(n)))

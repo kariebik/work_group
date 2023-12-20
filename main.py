7dbc1876695255f0c5104560106c61d4e9a2a60e
kariebik <kekatimi@gmail.com>
2023-12-20 10:25:54 -0500

    milestone 2

--- main.py
+++ main.py
@@ -13,33 +13,18 @@ label3 = Label(root, text="2: Find the fibonacci value").grid(row=2, column=0)
 label4 = Label(root, text= "Enter the number of the task you'd like to perform:").grid(row=3, column=0)
 
 e = Entry(root).grid(row=3, column=1, columnspan=2)
+n = int(e.get())
+def command():
+    if e.get() == 1:
+        label5 = Label(root, text="Enter the integer you'd like like to perform: ").grid(row=4, column=0)
+        e2 = Entry(root).grid(row=4, column=1, columnspan=2)
+        button = Button(root, text='get results', command= lambda: factorial(e2) )
+    elif e.get() == 2:
+        label5 = Label(root, text="Enter the sequence position of the fibonacci value: ").grid(row=4, column=0)
+        e2 = Entry(root).grid(row=4, column=1, columnspan=2)
+        button = Button(root, text='get results', command=lambda: fibonacci_sequence(e2))
 
-choice = e.get()
-
-if choice == 1:
-    label5 = Label(root, text="Enter the integer you'd like like to perform: ").grid(row=4, column=0)
-    e2 = Entry(root).grid(row=4, column=1, columnspan=2)
-    button = Button(root, text='get results', command= lambda: factorial(e2.get()) )
-elif choice == 2:
-    label5 = Label(root, text="Enter the sequence position of the fibonacci value: ").grid(row=4, column=0)
-    e2 = Entry(root).grid(row=4, column=1, columnspan=2)
-    button = Button(root, text='get results', command=lambda: fibonacci_sequence(e2.get()))
 
 root.mainloop()
 
-# if __name__ =='__main__':
-#     print("which task would you like to perform: ")
-#     print("1: Find the factorial")
-#     print("2: Find the fibonacci value")
-#     e = eval(input("Enter the number of the task you'd like to perform: "))
-#
-#     if e == 1:
-#         k = eval(input("Enter the integer you'd like like to perform: "))
-#         if not isinstance(k, int):
-#             raise TypeError
-#         print("The factorial of " + str(k) + " is: " + str(factorial(k)))
-#     elif e == 2:
-#         n = eval(input("Enter the sequence position of the fibonacci value: "))
-#         if not isinstance(n, int):
-#             raise TypeError
-#         print("The " + str(n) + " th fibonacci is " + str(fibonacci_sequence(n)))
+
